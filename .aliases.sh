92b70a2267b303b1f85040c09d7a5d135ed548fc
raz <mehraz.hakim@gmail.com>
2017-10-20 17:29:01 -0400

    add aliases file

---
+++ .aliases.sh
@@ -0,0 +1,12 @@
+
+# aliases
+
+alias bers='bundle exec rails s'
+alias berk='bundle exec rake'
+alias berc='bundle exec rails c'
+alias her='cd ~/dev/shotcaller/shotcaller-heroku'
+alias ion='cd ~/dev/shotcaller/shotcaller-client-ionic/shotcaller'
+alias scr='cd ~/dev/shotcaller'
+alias c='cordova'
+alias cba='cordova build android'
+alias cra='cordova run android'
